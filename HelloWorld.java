1fe473b458f8295edc6eedf42c1143e768cad994
Izzheree <faizargananta06@gmail.com>
2024-09-19 18:25:19 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Ananda Faiz Argananta");
+        System.out.println("NIM: 607062400033");
+        System.out.println("Kelas: D3IF4801");
+    }
+}
